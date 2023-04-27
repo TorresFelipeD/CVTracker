bd3175d9d59dd9d31578352074e291d6f46776fc
Diego Chaparro <torresfeliped@gmail.com>
2023-04-27 00:25:46 -0500

    Se crea crud de company

--- backend/Api/Controllers/CompanyController.cs
+++ backend/Api/Controllers/CompanyController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Database.Context;
+using Database.Models;
+using Newtonsoft.Json;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Controllers
 {
@@ -11,22 +14,112 @@ namespace Api.Controllers
     [Route("api/[controller]")]
     public class CompanyController : ControllerBase
     {
-        private readonly CVTrackerContext? _context;
-
-        public CompanyController(CVTrackerContext _context)
+        private readonly CVTrackerContext _context;
+        private readonly IConfiguration _configuration;
+        public CompanyController(CVTrackerContext _context, IConfiguration _configuration)
         {
             this._context = _context;
+            this._configuration = _configuration;
         }
 
         [HttpGet]
         public IActionResult Get()
         {
-            if (_context == null)
-                return StatusCode(404, new { Message = "Not Found Data" });
-            else if(_context?.Companies == null)
-                return StatusCode(404, new { Message = "Not Found Data in Companies" });
-            else
-                return Ok(_context?.Companies?.ToList());
+            try
+            {
+                return Ok(_context?.Companies?.ToList().OrderBy(x => x.Id).ToList());
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(500, new { MessageError = $"Error: {ex.Message}" });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(Company company)
+        {
+            try
+            {
+                _context?.Companies?.Add(company);
+                _context?.SaveChanges();
+                dynamic jsonObj = new
+                {
+                    Message = "Record created successfully",
+                    Record = company
+                };
+                return Ok(JsonConvert.SerializeObject(jsonObj, Formatting.Indented));
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(500, new { MessageError = $"Error: {ex.Message}" });
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Company company)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(id.ToString()))
+                {
+                    Company? row = _context?.Companies?.Where(x => x.Id == id).SingleOrDefault();
+                    if (row != null)
+                    {
+                        row.Name = company.Name;
+                        row.Description = company.Description;
+
+                        _context.Entry(row).State = EntityState.Modified;
+                        _context.SaveChanges();
+
+                        dynamic jsonObj = new
+                        {
+                            Message = "Record updated successfully",
+                            Record = row
+                        };
+                        return Ok(JsonConvert.SerializeObject(jsonObj, Formatting.Indented));
+                    }
+                    else
+                        return NotFound($"Record with Id:{id} Not Found");
+                }
+                else
+                    return BadRequest($"Field Id is required");
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(500, new { MessageError = $"Error: {ex.Message}" });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(id.ToString()))
+                {
+                    Company? row = _context?.Companies?.Where(x => x.Id == id).SingleOrDefault();
+                    if (row != null)
+                    {
+                        _context?.Companies?.Remove(row);
+                        _context.SaveChanges();
+
+                        dynamic jsonObj = new
+                        {
+                            Message = "Record deleted successfully",
+                            Record = row
+                        };
+                        return Ok(JsonConvert.SerializeObject(jsonObj, Formatting.Indented));
+                    }
+                    else
+                        return NotFound($"Record with Id:{id} Not Found");
+                }
+                else
+                    return BadRequest($"Field Id is required");
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(500, new { MessageError = $"Error: {ex.Message}" });
+            }
         }
     }
 }
--- backend/Api/Program.cs
+++ backend/Api/Program.cs
@@ -3,13 +3,28 @@ using Database.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var configBuilder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+    .AddEnvironmentVariables()
+    .Build();
+
+builder.WebHost.UseConfiguration(configBuilder);
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<CVTrackerContext>(op => op.UseInMemoryDatabase(databaseName: "cvtrackerDB"));
+
+// string test = builder.Configuration["AppSettings:Ambiente"].ToString();
+
+builder.Services.AddDbContext<CVTrackerContext>(op => {
+    op.UseInMemoryDatabase(databaseName: "cvtrackerDB");
+    //op.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    });
 // builder.WebHost.UseKestrel(serOp => {
 //     serOp.ListenAnyIP(6001);
 //     serOp.ListenAnyIP(6000, config => config.UseHttps());
