dece9e06249b81f6b472fb9614f1c5610f15a7f0
Diego Felipe Chaparro Torres <dchaparro@axede.com.co>
2023-05-04 10:14:44 -0500

    Se agregan metodos faltantes en el controlador de Job

--- backend/Api/Controllers/JobController.cs
+++ backend/Api/Controllers/JobController.cs
@@ -56,5 +56,97 @@ namespace Api.Controllers
                 return StatusCode(500, new { MessageError = $"Error: {ex.Message}" });
             }
         }
+
+        [HttpPost]
+        public IActionResult Post(Job Jobs)
+        {
+            try
+            {
+                _context?.Jobs?.Add(Jobs);
+                _context?.SaveChanges();
+                dynamic jsonObj = new
+                {
+                    Message = "Record created successfully",
+                    Record = Jobs
+                };
+                return Ok(JsonConvert.SerializeObject(jsonObj, Formatting.Indented));
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(500, new { MessageError = $"Error: {ex.Message}" });
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Job Jobs)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(id.ToString()))
+                {
+                    Job? row = _context?.Jobs?.Where(x => x.Id == id).SingleOrDefault();
+                    if (row != null)
+                    {
+                        row.IdCompany = Jobs.IdCompany;
+                        row.PositionName = Jobs.PositionName;
+                        row.IdSkills = Jobs.IdSkills;
+                        row.IdRequirements = Jobs.IdRequirements;
+                        row.IdEmploymentExchange = Jobs.IdEmploymentExchange;
+                        row.JobPostingURL = Jobs.JobPostingURL;
+                        row.IdStatus = Jobs.IdStatus;
+
+                        _context.Entry(row).State = EntityState.Modified;
+                        _context.SaveChanges();
+
+                        dynamic jsonObj = new
+                        {
+                            Message = "Record updated successfully",
+                            Record = row
+                        };
+                        return Ok(JsonConvert.SerializeObject(jsonObj, Formatting.Indented));
+                    }
+                    else
+                        return NotFound($"Record with Id:{id} Not Found");
+                }
+                else
+                    return BadRequest($"Field Id is required");
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(500, new { MessageError = $"Error: {ex.Message}" });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(id.ToString()))
+                {
+                    Job? row = _context?.Jobs?.Where(x => x.Id == id).SingleOrDefault();
+                    if (row != null)
+                    {
+                        _context?.Jobs?.Remove(row);
+                        _context.SaveChanges();
+
+                        dynamic jsonObj = new
+                        {
+                            Message = "Record deleted successfully",
+                            Record = row
+                        };
+                        return Ok(JsonConvert.SerializeObject(jsonObj, Formatting.Indented));
+                    }
+                    else
+                        return NotFound($"Record with Id:{id} Not Found");
+                }
+                else
+                    return BadRequest($"Field Id is required");
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(500, new { MessageError = $"Error: {ex.Message}" });
+            }
+        }
     }
 }
